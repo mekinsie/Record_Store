067b222e5ea38931d3b5df70ba4b2217ebe3d6b2
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-01 14:18:34 -0800

    Record Store Sinatra Project
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source('https://rubygems.org')
+
+gem('sinatra-contrib')
+gem('sinatra')
+gem('pry')
+gem('rspec')
---
+++ app.rb
@@ -0,0 +1,64 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/album')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums') do
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/albums/new') do
+  erb(:new_album)
+end
+
+get('/albums/:id') do
+  @album = Album.find(params[:id].to_i)
+  erb(:album)
+end
+
+post('/albums') do
+  name = params[:album_name]
+  album = Album.new(name, nil)
+  album.save()
+  @albums = Album.all()
+  erb(:albums)
+end
+
+get('/albums/:id/edit') do
+  @album = Album.find(params[:id].to_i())
+  erb(:edit_album)
+end
+
+patch('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.update(params[:name])
+  @albums = Album.all
+  erb(:albums)
+end
+
+delete('/albums/:id') do
+  @album = Album.find(params[:id].to_i())
+  @album.delete()
+  @albums = Album.all
+  erb(:albums)
+end
+
+get('/custom_route') do
+  "We can even create custom routes, but we should only do this when needed."
+end
+
+get('/best_album_ever') do
+  "This will show the best album that has ever existed in all time."
+end
+
+get('/test') do
+  @something = "this is a variable"
+  erb(:whatever)
+end
